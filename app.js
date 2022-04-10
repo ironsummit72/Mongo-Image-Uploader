11f79a201d5f52fedbec2bb5f6f31ddd87aa353f
ironsummit72 <ironsumit72@gmail.com>
2022-04-11 01:20:45 +0530

    database data insertion successfull

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const multer = require("multer");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
-const port = 80;
+const port = 800;
 
 const app = express();
+
+// this is multer a bridge between file uploads
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, './uploads')
@@ -17,6 +20,18 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage: storage })
+
+// connection with the database
+
+mongoose.connect("mongodb://localhost:27017/mongo-img");
+var db = mongoose.connection;
+db.on("error", console.log.bind(console, "connection error"));
+db.once("open", function (callback) {
+  console.log("connection succeeded");
+});
+
+
+
 // using body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 // setting view engine
@@ -32,7 +47,21 @@ app.get("/upload", (req, res) => {
   res.render("upload")
 });
 app.post("/upload",upload.single('uploadimg'), (req, res) => {
-  console.log(req.file);
+  console.log(req.file); 
+
+  let path=req.file.path;
+  let filename=req.file.filename;
+  
+
+  
+  // inserting data into the database
+
+  let schema = { Filename: filename,PATH: path };
+  db.collection("imgcol").insertOne(schema, function (err, collection) {
+    if (err) throw err;
+    console.log("record insert successfully");
+  });
+
 
 })
 
