f17ac95caf27423b5ef5f091b6a047090244449f
ironsummit72 <ironsumit72@gmail.com>
2022-04-15 13:40:33 +0530

    added clear all feature

--- app.js
+++ app.js
@@ -7,33 +7,32 @@ var MongoClient = require("mongodb").MongoClient;
 const fs = require("fs");
 const port = 800;
 var url = "mongodb://localhost:27017/";
-const dir = './uploads';
+const dir = "./uploads";
 
 const app = express();
-var imgpaths=[]; 
-    var item='';
-// this is multer a bridge between file uploads
-
+var imgpaths = [];
+var item = "";
 
 // checking if uploads folder exists
 if (fs.existsSync(dir)) {
-  console.log('Directory exists!');
+  console.log("Directory exists!");
 } else {
-  console.log('Directory not found.');
-  fs.mkdirSync(dir)
+  console.log("Directory not found.");
+  fs.mkdirSync(dir);
 }
 
+// this is multer a bridge between file uploads
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './uploads')
+    cb(null, "./uploads");
   },
   filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    cb(null, file.fieldname + '-'+file.originalname)
-  }
-})
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + file.originalname);
+  },
+});
 
-const upload = multer({ storage: storage })
+const upload = multer({ storage: storage });
 
 // connection with the database
 
@@ -44,24 +43,21 @@ db.once("open", function (callback) {
   console.log("connection succeeded");
 });
 
-
-
 // using body parser
 app.use(bodyParser.urlencoded({ extended: true }));
 // setting view engine
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname,'public')));
-app.use('/uploads', express.static('uploads'));
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/uploads", express.static("uploads"));
 
 app.get("/", (req, res) => {
+  //reciving data from database
 
-   //reciving data from database
-
-   MongoClient.connect(url, function (err, db) {
+  MongoClient.connect(url, function (err, db) {
     if (err) throw err;
     var dbo = db.db("mongo-img"); //database name
     dbo
-      .collection("imgcol")  //collection name
+      .collection("imgcol") //collection name
       .find({})
       .toArray(function (err, result) {
         if (err) throw err;
@@ -76,39 +72,52 @@ app.get("/", (req, res) => {
       });
   });
 
- res.render("index", {Imagepath:imgpaths})
+  res.render("index", { Imagepath: imgpaths });
 });
 
 app.get("/upload", (req, res) => {
-
   res.render("upload");
-
 });
-app.post("/upload",upload.single('uploadimg'), (req, res) => {
-  console.log(req.file); 
+app.post("/upload", upload.single("uploadimg"), (req, res) => {
+  console.log(req.file);
 
-  let path=req.file.path; 
-  let filename=req.file.filename;
-  
+  let path = req.file.path;
+  let filename = req.file.filename;
 
-  
   // inserting data into the database
 
-  let schema = { Filename: filename,PATH: path };
+  let schema = { Filename: filename, PATH: path };
   db.collection("imgcol").insertOne(schema, function (err, collection) {
     if (err) throw err;
     console.log("record insert successfully");
   });
-  
-  res.render('uploadsuccess')
 
+  res.render("uploadsuccess");
+});
 
-})
+//  clearing all the data form the server as well as database
 
+app.get("/clearall", (req, res) => {
+  //  clearing all data form database
+  MongoClient.connect(url, function (err, db) {
+    if (err) throw err;
+    var dbo = db.db("mongo-img"); //database name
+    dbo
+      .collection("imgcol") //collection name
+      .deleteMany({});
+  });
 
+  // removing data form folder
+  fs.readdir(dir, (err, files) => {
+    if (err) throw err;
 
-
-app.post("/", (req, res) => {});
+    for (const file of files) {
+      fs.unlink(path.join(dir, file), (err) => {
+        if (err) throw err;
+      });
+    }
+  });
+});
 
 app.listen(port, function () {
   console.log(`listening on ${port}`);
